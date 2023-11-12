555da6d55b54e6b7a2f20d620b60d9ecd1276f13
tle <nantuko_titan@hotmail.com>
2023-11-12 13:37:16 +0700

    edit msg landingpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,32 +11,34 @@ export default function Home() {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
+        <li><a>Page</a></li>
         <li>
-          <a>Parent</a>
+          <a>Gallery</a>
           <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
+            <li><a>Gallery  1</a></li>
+            <li><a>Gallery 2</a></li>
           </ul>
         </li>
-        <li><a>Item 3</a></li>
+        <li><a>Shop</a></li>
+        <li><a>About Us</a></li>
       </ul>
     </div>
-    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+    <a className="btn btn-ghost normal-case text-xl">FUEGO</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
+      <li><a>Page</a></li>
       <li tabIndex={0}>
         <details>
-          <summary>Parent</summary>
+          <summary>Gallery</summary>
           <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
+            <li><a>Gallery 1</a></li>
+            <li><a>Gallery 2</a></li>
           </ul>
         </details>
       </li>
-      <li><a>Item 3</a></li>
+      <li><a>Shop</a></li>
+      <li><a>About Us</a></li>
     </ul>
   </div>
   <div className="navbar-end">
@@ -48,9 +50,9 @@ export default function Home() {
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-center text-neutral-content">
     <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
+      <h1 className="mb-5 text-5xl font-bold">Out there</h1>
+      <p className="mb-5">Typically, fall in the Canadian Rocky Mountains lasts only a few short weeks. Mixed in with all that beautiful colour is a variety of lighting and weather conditions.</p>
+      <button className="btn btn-primary">Gallery</button>
     </div>
   </div>
 </div>
@@ -59,9 +61,9 @@ export default function Home() {
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-center text-neutral-content">
     <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
+      <h1 className="mb-5 text-5xl font-bold">Immersion</h1>
+      <p className="mb-5">When it comes to landscape photography, most of you will agree that location matters. It’s well worth the effort to research and scout locations you intend to photograph, especially if you seek a specific element, like fall colour, within your images. Not all locations will offer fall colours in abundance. </p>
+      <button className="btn btn-primary">Shop</button>
     </div>
   </div>
 </div>
@@ -70,9 +72,9 @@ export default function Home() {
   <div className="hero-overlay bg-opacity-60"></div>
   <div className="hero-content text-center text-neutral-content">
     <div className="max-w-md">
-      <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-      <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
+      <h1 className="mb-5 text-5xl font-bold">To Infinity</h1>
+      <p className="mb-5">As in everything to do with photography, good light can make or break a photo. Prime light for landscape photography is around the hours of sunrise and sunset. However, one of the things I love about fall is that this season looks spectacular in all types of light. Don’t put your camera away during the day hours.</p>
+      <button className="btn btn-primary">About Us</button>
     </div>
   </div>
 </div>
@@ -80,18 +82,15 @@ export default function Home() {
 
     <footer className="footer p-10 bg-neutral text-neutral-content">
   <nav>
-    <header className="footer-title">Services</header> 
+    <header className="footer-title">Gallery</header> 
     <a className="link link-hover">Branding</a>
     <a className="link link-hover">Design</a>
-    <a className="link link-hover">Marketing</a>
-    <a className="link link-hover">Advertisement</a>
+    <a className="link link-hover">Shop</a>
   </nav> 
   <nav>
     <header className="footer-title">Company</header> 
     <a className="link link-hover">About us</a>
     <a className="link link-hover">Contact</a>
-    <a className="link link-hover">Jobs</a>
-    <a className="link link-hover">Press kit</a>
   </nav> 
   <nav>
     <header className="footer-title">Legal</header> 
